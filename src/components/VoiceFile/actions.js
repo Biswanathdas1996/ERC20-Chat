a376a8592181fefa4e6f40b00a24b765a060ee5d
your_username <your_email_address@example.com>
2022-05-30 23:23:06 +0530

    ok

--- src/components/VoiceFile/actions.js
+++ src/components/VoiceFile/actions.js
@@ -2,10 +2,16 @@ export function callSupport() {
   return window.open("tel:8001691299");
 }
 
+export function chatOnWhatsApp() {
+  return window.open("https://api.whatsapp.com/send?phone=8001691299&text=Hi");
+}
+
 export function actionItems(action) {
   switch (action) {
     case "callSupport":
       return callSupport;
+    case "chatOnWhatsApp":
+      return chatOnWhatsApp;
 
     default:
     // code block
